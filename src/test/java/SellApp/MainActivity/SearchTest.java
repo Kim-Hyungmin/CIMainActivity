9b863c4e11fe0f61559f97a77f1d8e32cb7fb0cc
Kim-Hyungmin <khmin6998@gmail.com>
2020-12-09 16:32:16 +0900

    FirstTest

---
+++ src/test/java/SellApp/MainActivity/SearchTest.java
@@ -0,0 +1,20 @@
+package SellApp.MainActivity;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class SearchTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+	@Test
+	public void testSearch() {
+		Search s = new Search();
+		assertNotNull(s);
+	}
+}
